c2797116544bdf0962463828407bca4062f0b480
caiofaria6227-blip <caiofaria6227@gmail.com>
2026-03-02 22:46:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,39 @@
-self.addEventListener("install", e=>{
-e.waitUntil(
-caches.open("dom-cache").then(cache=>{
-return cache.addAll([
-"index.html",
-"manifest.json",
-"icon-192.png",
-"icon-512.png"
-]);
-})
-);
+const CACHE_NAME = "dom-miniaturas-cache-v2";
+
+// Arquivos que devem ser atualizados sempre que você modifica o site
+const urlsToCache = [
+  "/dom-miniaturas/index.html?v=2",
+  "/dom-miniaturas/manifest.json",
+  "/dom-miniaturas/icon-192.png",
+  "/dom-miniaturas/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", e=>{
-e.respondWith(
-caches.match(e.request).then(res=>res||fetch(e.request))
-);
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
